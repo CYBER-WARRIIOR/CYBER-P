b05782aa40416542e06abace5c4b6dbe0fcaf23d
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2025-02-17 23:39:41 +0500

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,6 +1,7 @@
 import { spawn } from 'child_process'
-let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+let handler = async (m, { conn }) => {
+    let thumbnail = 'https://www.guruapi.tech/K.jpg'
+  let fgg = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `GURU-BOT`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'GURU-BOT'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     if (conn.user.jid == conn.user.jid) {
     await m.reply('_*♻️Restarting Bot...*_\n _*Wait a moment*_')
     process.send('reset')
